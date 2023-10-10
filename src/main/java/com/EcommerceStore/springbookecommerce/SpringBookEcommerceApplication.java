9e22ab244be18dc774e9f317723123ca7be0c217
dbenitez02 <dbenitez101@gmail.com>
2023-10-10 00:20:50 -0700

    first commit, added Product and ProductCategory entities

---
+++ src/main/java/com/EcommerceStore/springbookecommerce/SpringBookEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.EcommerceStore.springbookecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBookEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBookEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/EcommerceStore/springbookecommerce/SpringBookEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EcommerceStore.springbookecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBookEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
