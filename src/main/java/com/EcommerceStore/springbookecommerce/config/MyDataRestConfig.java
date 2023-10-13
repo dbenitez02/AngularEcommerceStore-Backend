3de626a6c64fc784e921dad61dcb958854c69de6
dbenitez02 <36527173+D-Benz@users.noreply.github.com>
2023-10-13 01:03:48 -0700

    Added configuration to expose ids for product cagetory

--- src/main/java/com/EcommerceStore/springbookecommerce/config/MyDataRestConfig.java
+++ src/main/java/com/EcommerceStore/springbookecommerce/config/MyDataRestConfig.java
@@ -3,15 +3,30 @@ package com.EcommerceStore.springbookecommerce.config;
 import com.EcommerceStore.springbookecommerce.dao.ProductRepository;
 import com.EcommerceStore.springbookecommerce.entity.Product;
 import com.EcommerceStore.springbookecommerce.entity.ProductCategory;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.metamodel.EntityType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
     private String theAllowedOrigins = "http://localhost:4200";
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public MyDataRestConfig(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
 
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
 
@@ -44,5 +59,26 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
                 .forDomainType(theClass)
                 .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedMethods))
                 .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedMethods));
+
+        // Call an internal helper method
+        exposedIds(config);
+    }
+
+    public void exposedIds(RepositoryRestConfiguration config) {
+
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+
+        // Get a list of all entity classes from the entity manager
+        List<Class> entityClasses = new ArrayList<>();
+
+        // Create an array of the entity types
+        for (EntityType tempEntityType : entities) {
+            entityClasses.add(tempEntityType.getJavaType());
+        }
+
+        // expose the entity ids for the array of entity/domain type
+        Class[] domainTypes = entityClasses.toArray(new Class[0]);
+        config.exposeIdsFor(domainTypes);
+
     }
 }
