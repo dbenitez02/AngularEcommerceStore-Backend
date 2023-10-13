a6145b501160465f7ddbd2c352c8adc30477b775
dbenitez02 <36527173+D-Benz@users.noreply.github.com>
2023-10-13 00:24:01 -0700

    enhanced ProductRepository

--- src/main/java/com/EcommerceStore/springbookecommerce/dao/ProductRepository.java
+++ src/main/java/com/EcommerceStore/springbookecommerce/dao/ProductRepository.java
@@ -1,8 +1,11 @@
 package com.EcommerceStore.springbookecommerce.dao;
 
 import com.EcommerceStore.springbookecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-
+    Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
 }
