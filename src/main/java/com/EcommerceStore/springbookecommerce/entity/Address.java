7b7c1a7db6c8d9a936000cb9f5f764880400d145
dbenitez02 <36527173+D-Benz@users.noreply.github.com>
2023-11-16 01:08:29 -0800

    Added entities for Address, Customer, Order, and OrderItem

---
+++ src/main/java/com/EcommerceStore/springbookecommerce/entity/Address.java
@@ -0,0 +1,34 @@
+package com.EcommerceStore.springbookecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "address")
+@Data
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "street")
+    private String street;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "state")
+    private String state;
+
+    @Column(name = "country")
+    private String country;
+
+    @Column(name = "zipcode")
+    private String zipcode;
+
+    @OneToOne
+    @PrimaryKeyJoinColumn
+    private Order order;
+}
